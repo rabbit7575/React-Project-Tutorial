0890d9a90ced3794c1f52879cb2dab5c9d287f80
siyoung <raindrpo891@naver.com>
2021-01-31 23:24:58 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -18,25 +19,36 @@ const styles = theme => ({
   },
   talbe : {
     minWidth : 1080
+  },
+  progress : {
+    margin : theme.spacing.unit * 2
   }
 })
 
 class App extends Component {
 
   state = {
-    customers : ""
+    customers : "",
+    completed : 0
 
   }
  componentDidMount(){
+   this.timer = setInterval(this.progress, 20);
    this.callApi()
-    .then(res => this.setState({customers:res}))
-    .catch(err => console.log(err));
+   .then(res => this.setState({customers:res}))
+   .catch(err => console.log(err));
  }
  callApi = async () => {
    const response = await fetch('/api/customers');
    const body = await response.json();
    return body;
  }
+
+ progress = () =>{
+   const {completed} = this.state;
+   this.setState({ completed : completed >= 100 ? 0 : completed + 1 })
+ }
+
   render(){
     const { classes } = this.props;
     return (
@@ -66,7 +78,12 @@ class App extends Component {
                     job={c.job}
                   />
                 );
-              }) : ""
+              }) : 
+              <TableRow>
+                <TableCell colspan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
             }
           </TableBody>
         </Table>
